bf84b2bec73cf62b448db4338433ecaa337c699b
RageCodeGames <88705730+RageCodeGames@users.noreply.github.com>
2026-02-08 14:22:55 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,54 @@
-const CACHE_NAME = 'pin-app-v1';
+const CACHE_NAME = 'pin-app-v2';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
+  './icon-72.png',
+  './icon-96.png',
+  './icon-128.png',
+  './icon-144.png',
+  './icon-152.png',
+  './icon-192.png',
+  './icon-384.png',
+  './icon-512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
 ];
 
+// Install event
 self.addEventListener('install', function(event) {
+  console.log('[ServiceWorker] Install');
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
+        console.log('[ServiceWorker] Caching app shell');
         return cache.addAll(urlsToCache);
+      })
+      .then(function() {
+        return self.skipWaiting();
       })
   );
 });
 
+// Activate event
+self.addEventListener('activate', function(event) {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+// Fetch event
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
@@ -22,22 +56,22 @@ self.addEventListener('fetch', function(event) {
         if (response) {
           return response;
         }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
+        return fetch(event.request).then(function(response) {
+          // Don't cache non-successful responses
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
           }
-        })
-      );
-    })
+          
+          // Clone the response
+          var responseToCache = response.clone();
+          
+          caches.open(CACHE_NAME)
+            .then(function(cache) {
+              cache.put(event.request, responseToCache);
+            });
+          
+          return response;
+        });
+      })
   );
 });
